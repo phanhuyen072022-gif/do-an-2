7cb89ba13cd12a9a769b7790f53209ea9e1a7824
phanhuyen072022-gif <phanhuyen072022@gmail.com>
2026-02-14 16:04:24 +0700

    chức năng khách hàng page sp

--- razor-page/razor-page/Pages/Index.cshtml.cs
+++ razor-page/razor-page/Pages/Index.cshtml.cs
@@ -1,20 +1,28 @@
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using razor_page.Models;
+using razor_page.Services;
 
 namespace razor_page.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly ServiceHoa _serviceHoa;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        // Danh sách hoa để truyền ra ngoài View
+        public List<Hoa> DanhSachHoaNoiBat { get; set; } = new List<Hoa>();
+
+        public IndexModel(ILogger<IndexModel> logger, ServiceHoa serviceHoa)
         {
             _logger = logger;
+            _serviceHoa = serviceHoa;
         }
 
         public void OnGet()
         {
-
+            // Lấy 5 sản phẩm đầu tiên hoặc mới nhất từ CSDL cho phần nổi bật
+            DanhSachHoaNoiBat = _serviceHoa.GetAll().Take(5).ToList();
         }
     }
 }
---
+++ razor-page/razor-page/Pages/SanPham.cshtml.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using razor_page.Models;
+using razor_page.Services;
+
+namespace razor_page.Pages
+{
+    public class SanPhamModel : PageModel
+    {
+        private readonly ServiceHoa _serviceHoa;
+
+        public SanPhamModel(ServiceHoa serviceHoa)
+        {
+            _serviceHoa = serviceHoa;
+        }
+
+        public List<Hoa> DanhSachHoa { get; set; } = new List<Hoa>();
+
+        [BindProperty(SupportsGet = true)]
+        public string SearchQuery { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string SortOrder { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public decimal? MinPrice { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public decimal? MaxPrice { get; set; }
+
+        public void OnGet()
+        {
+            // Lấy danh sách từ service
+            var query = _serviceHoa.GetAll().AsEnumerable();
+
+            // 1. Lọc theo tên (Search)
+            if (!string.IsNullOrWhiteSpace(SearchQuery))
+            {
+                query = query.Where(h => h.TenHoa.Contains(SearchQuery, StringComparison.OrdinalIgnoreCase));
+            }
+
+            // 2. Lọc theo giá (Filter)
+            if (MinPrice.HasValue)
+            {
+                query = query.Where(h => h.GiaBan >= MinPrice.Value);
+            }
+            if (MaxPrice.HasValue)
+            {
+                query = query.Where(h => h.GiaBan <= MaxPrice.Value);
+            }
+
+            // 3. Sắp xếp (Sort)
+            switch (SortOrder)
+            {
+                case "price_asc":
+                    query = query.OrderBy(h => h.GiaBan);
+                    break;
+                case "price_desc":
+                    query = query.OrderByDescending(h => h.GiaBan);
+                    break;
+                case "name_asc":
+                    query = query.OrderBy(h => h.TenHoa);
+                    break;
+                case "name_desc":
+                    query = query.OrderByDescending(h => h.TenHoa);
+                    break;
+                default:
+                    // Mặc định hiển thị hoa mới nhất (đảo ngược ID)
+                    query = query.OrderByDescending(h => h.HoaId);
+                    break;
+            }
+
+            DanhSachHoa = query.ToList();
+        }
+    }
+}
